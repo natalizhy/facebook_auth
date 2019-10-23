9548444610697eaf3a7a6e94fe9c06375af77d4d
Natali Zhylina <natalizhylina@Natalis-MacBook-Pro.local>
2019-10-23 12:52:22 +0300

    Get facebook profile

--- auth/main.go
+++ auth/main.go
@@ -1,39 +1,31 @@
 package main
 
 import (
+	"bytes"
+	"encoding/json"
 	"fmt"
 	"github.com/antonholmquist/jason"
 	"golang.org/x/oauth2"
 	"net/http"
-	"strconv"
-	"strings"
 )
 
 type AccessToken struct {
-	Token  string
-	Expiry int64
+	Access_token  string
+	Expires_in int64
 }
 
 func readHttpBody(response *http.Response) string {
-
 	fmt.Println("Reading body")
 
-	bodyBuffer := make([]byte, 5000)
-	var str string
-
-	count, err := response.Body.Read(bodyBuffer)
-
-	for ; count > 0; count, err = response.Body.Read(bodyBuffer) {
-
-		if err != nil {
-
-		}
-
-		str += string(bodyBuffer[:count])
+	buf := bytes.Buffer{}
+	_, err := buf.ReadFrom(response.Body)
+	if err != nil{
+		fmt.Println(err)
 	}
 
-	return str
+	fmt.Println(buf.String())
 
+	return buf.String()
 }
 
 //Converts a code to an Auth_Token
@@ -45,18 +37,13 @@ func GetAccessToken(client_id string, code string, secret string, callbackUri st
 		"&client_secret=" + secret + "&code=" + code)
 
 	if err == nil {
-
 		auth := readHttpBody(response)
 
 		var token AccessToken
 
-		tokenArr := strings.Split(auth, "&")
-
-		token.Token = strings.Split(tokenArr[0], "=")[1]
-		expireInt, err := strconv.Atoi(strings.Split(tokenArr[1], "=")[1])
-
-		if err == nil {
-			token.Expiry = int64(expireInt)
+		err := json.Unmarshal([]byte(auth), &token)
+		if err != nil {
+			fmt.Println(err)
 		}
 
 		return token
@@ -78,13 +65,12 @@ func Home(w http.ResponseWriter, r *http.Request) {
 		ClientID:     "408565436483487", // change this to yours
 		ClientSecret: "650844db51055f30e7e3721e7396f97c",
 		RedirectURL:  "http://localhost:8080/FBLogin", // change this to your webserver adddress
-		Scopes:       []string{"email", "user_birthday", "user_location", "user_about_me"},
+		Scopes:       []string{"email", "user_birthday", "user_location", "user_photos"},
 		Endpoint: oauth2.Endpoint{
 			AuthURL:  "https://www.facebook.com/dialog/oauth",
 			TokenURL: "https://graph.facebook.com/oauth/access_token",
 		},
 	}
-
 	url := fbConfig.AuthCodeURL("")
 
 	// Home page will display a button for login to Facebook
@@ -98,13 +84,14 @@ func FBLogin(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html; charset=utf-8")
 	code := r.FormValue("code")
 
-	ClientId := "" // change this to yours
-	ClientSecret := ""
+	ClientId := "408565436483487" // change this to yours
+	ClientSecret := "650844db51055f30e7e3721e7396f97c"
 	RedirectURL := "http://localhost:8080/FBLogin"
 
 	accessToken := GetAccessToken(ClientId, code, ClientSecret, RedirectURL)
 
-	response, err := http.Get("https://graph.facebook.com/me?access_token=" + accessToken.Token)
+	response, err := http.Get("https://graph.facebook.com/me?fields=id,name,birthday,email&access_token=" + accessToken.Access_token)
+	//response, err := http.Get("https://graph.facebook.com/me?access_token=" + accessToken.Access_token)
 
 	// handle err. You need to change this into something more robust
 	// such as redirect back to home page with error message
@@ -113,17 +100,21 @@ func FBLogin(w http.ResponseWriter, r *http.Request) {
 	}
 
 	str := readHttpBody(response)
-
-	// dump out all the data
-	// w.Write([]byte(str))
+	//dump out all the data
+	//w.Write([]byte(str))
 
 	// see https://www.socketloop.com/tutorials/golang-process-json-data-with-jason-package
-	user, _ := jason.NewObjectFromBytes([]byte(str))
+	user, err := jason.NewObjectFromBytes([]byte(str))
+	if err != nil {
+		fmt.Println(err)
+	}
 
 	id, _ := user.GetString("id")
 	email, _ := user.GetString("email")
 	bday, _ := user.GetString("birthday")
-	fbusername, _ := user.GetString("username")
+	fbusername, _ := user.GetString("name")
+
+	fmt.Println("id:", id, "email", email, "birthday", bday, "name", fbusername, )
 
 	w.Write([]byte(fmt.Sprintf("Username %s ID is %s and birthday is %s and email is %s<br>", fbusername, id, bday, email)))
 
@@ -141,5 +132,8 @@ func main() {
 	mux.HandleFunc("/", Home)
 	mux.HandleFunc("/FBLogin", FBLogin)
 
-	http.ListenAndServe(":8080", mux)
+	addr := http.ListenAndServe(":8080", mux)
+	if addr != nil {
+		fmt.Println(addr)
+	}
 }
